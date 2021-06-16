618640ca8cc1c5042d9e4c1ac34e688da4d9fb47
Amilton Santos <68564982+AmiltonSantos@users.noreply.github.com>
2021-06-16 19:35:40 -0300

    :art: Atualizando o app

--- project-api-nodejs-002/app.js
+++ project-api-nodejs-002/app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-  res.status(200).send({
-    mensagem: 'Ok, Deu certo'
-  });
-});
+const rotaProdutos = require('./routes/produtos');
 
+app.use('/produto', rotaProdutos);
+ 
 console.log('Servidor iniciado com sucesso')
 
 module.exports = app;
