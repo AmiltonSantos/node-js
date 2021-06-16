50b6211dce88b297d50a5f6a032406b361d3f2e7
Amilton Santos <68564982+AmiltonSantos@users.noreply.github.com>
2021-06-16 19:34:45 -0300

    :art: Atualizando o arquivo produto

--- project-api-nodejs-002/routes/produtos.js
+++ project-api-nodejs-002/routes/produtos.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const router = express.Router();
 
+router.get('/', (req, res, next) => {
+  res.status(200).send({
+    mensagem:  'Usando GET dentro de produto'
+  })
+});
+
+router.post('/', (req, res, next) => {
+  res.status(201).send({
+    mensagem:  'Usando POST dentro de produto'
+  })
+});
+
+module.exports = router;
